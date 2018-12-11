27320a89d6950be1bbd9d1a7832b291227fe58d2
y-che-n <ychen374@illinois.edu>
2018-12-11 11:02:42 -0600

    242 week 2-4

--- source/components/events/events.jsx
+++ source/components/events/events.jsx
@@ -15,16 +15,16 @@ class Events extends Component {
     }
 
     componentWillMount() {
-        axios.get('http://points-api.illinoiswcs.org/api/events').then( (response) => {
+        axios.get('http://localhost:3000/api/events').then( (response) => {
 
             let events = response.data.data;
             events.sort(function(a, b) {
-                var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-                var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-                if (nameA < nameB) {
+                var dateA = new Date(a.date).getTime();
+                var dateB = new Date(b.date).getTime();
+                if (dateA < dateB) {
                   return -1;
                 }
-                if (nameA > nameB) {
+                if (dateA > dateB) {
                   return 1;
                 }
 
@@ -42,22 +42,127 @@ class Events extends Component {
 
         const events = this.state.events.map( (event) => {
 
-            const attendees = event.attendees.map( (attendee) => {
-                return(<Label>{attendee}</Label>)
-            })
+            if (event.category === "Corporate") {
+              return(
+                  <Segment className="Events__corp" padded>
 
-            const panels = [
-              {
-                title: 'Show Attendees',
-                content: {
-                  content: (
-                      <Segment className="Events__attendees">
-                          {attendees}
-                      </Segment>
-                  )
-                }
-              },
-            ]
+                      <div className="Events__flex">
+                          <div className="Events__flexItem">
+
+                              <h3>{event.name}</h3>
+
+                              <h5 className="muted">{ new Date(event.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"}) }</h5>
+                          </div>
+                          <div className="Events__flexItem">
+                              <Statistic className="Events_statistic" size='tiny'>
+                                <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                                <Statistic.Label>Attended</Statistic.Label>
+                              </Statistic>
+                          </div>
+                      </div>
+                  </Segment>
+              )
+            } else if (event.category === "General Meeting") {
+              return(
+                  <Segment className="Events__gm" padded>
+
+                      <div className="Events__flex">
+                          <div className="Events__flexItem">
+
+                              <h3>{event.name}</h3>
+
+                              <h5 className="muted">{ new Date(event.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"}) }</h5>
+                          </div>
+                          <div className="Events__flexItem">
+                              <Statistic className="Events_statistic" size='tiny'>
+                                <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                                <Statistic.Label>Attended</Statistic.Label>
+                              </Statistic>
+                          </div>
+                      </div>
+                  </Segment>
+              )
+            } else if (event.category === "Tech Team") {
+              return(
+                  <Segment className="Events__techTeam" padded>
+
+                      <div className="Events__flex">
+                          <div className="Events__flexItem">
+
+                              <h3>{event.name}</h3>
+
+                              <h5 className="muted">{ new Date(event.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"}) }</h5>
+                          </div>
+                          <div className="Events__flexItem">
+                              <Statistic className="Events_statistic" size='tiny'>
+                                <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                                <Statistic.Label>Attended</Statistic.Label>
+                              </Statistic>
+                          </div>
+                      </div>
+                  </Segment>
+              )
+            } else if (event.category === "Social") {
+              return(
+                  <Segment className="Events__social" padded>
+
+                      <div className="Events__flex">
+                          <div className="Events__flexItem">
+
+                              <h3>{event.name}</h3>
+
+                              <h5 className="muted">{ new Date(event.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"}) }</h5>
+                          </div>
+                          <div className="Events__flexItem">
+                              <Statistic className="Events_statistic" size='tiny'>
+                                <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                                <Statistic.Label>Attended</Statistic.Label>
+                              </Statistic>
+                          </div>
+                      </div>
+                  </Segment>
+              )
+            } else if (event.category === "Mentoring") {
+              return(
+                  <Segment className="Events__mentoring" padded>
+
+                      <div className="Events__flex">
+                          <div className="Events__flexItem">
+
+                              <h3>{event.name}</h3>
+
+                              <h5 className="muted">{ new Date(event.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"}) }</h5>
+                          </div>
+                          <div className="Events__flexItem">
+                              <Statistic className="Events_statistic" size='tiny'>
+                                <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                                <Statistic.Label>Attended</Statistic.Label>
+                              </Statistic>
+                          </div>
+                      </div>
+                  </Segment>
+              )
+            } else if (event.category === "Outreach") {
+              return(
+                  <Segment className="Events__outreach" padded>
+
+                      <div className="Events__flex">
+                          <div className="Events__flexItem">
+
+                              <h3>{event.name}</h3>
+
+                              <h5 className="muted">{ new Date(event.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"}) }</h5>
+                          </div>
+                          <div className="Events__flexItem">
+                              <Statistic className="Events_statistic" size='tiny'>
+                                <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                                <Statistic.Label>Attended</Statistic.Label>
+                              </Statistic>
+                          </div>
+                      </div>
+                  </Segment>
+              )
+            } else {
 
             return(
                 <Segment className="Events__event" padded>
@@ -68,8 +173,6 @@ class Events extends Component {
                             <h3>{event.name}</h3>
 
                             <h5 className="muted">{ new Date(event.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"}) }</h5>
-
-                            <Accordion panels={panels} />
                         </div>
                         <div className="Events__flexItem">
                             <Statistic className="Events_statistic" size='tiny'>
@@ -80,6 +183,7 @@ class Events extends Component {
                     </div>
                 </Segment>
             )
+          }
         })
 
         return(
